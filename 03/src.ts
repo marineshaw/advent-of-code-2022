327e5eb13042dadf52c52ddbc8ba8a575dc0a1b9
marineshaw <marines@bam.tech>
2022-12-05 23:14:17 +0100

    :sparkles: (DayThree) Part 2

--- 03/src.ts
+++ 03/src.ts
@@ -51,11 +51,67 @@ const computeTotalLostItemPriorities = (rucksacksList: Rucksack[]): number => {
   return total;
 };
 
+const createElvGroups = (rucksacksList: Rucksack[]): ElfGroup[] => {
+  const elfGroups: ElfGroup[] = [];
+  const elfGroupLenght = rucksacksList.length / 3;
+
+  for (let i = 0; i < elfGroupLenght; i++) {
+    const elfGroup: ElfGroup = [
+      rucksacksList[i * 3],
+      rucksacksList[i * 3 + 1],
+      rucksacksList[i * 3 + 2],
+    ];
+    elfGroups.push(elfGroup);
+  }
+
+  return elfGroups;
+};
+
+const findCommonItemInElvGroup = (elfGroup: ElfGroup): string => {
+  const firstElfRucksack = elfGroup[0].compartments[0].concat(
+    elfGroup[0].compartments[1]
+  );
+  const secondElfRucksack = elfGroup[1].compartments[0].concat(
+    elfGroup[1].compartments[1]
+  );
+  const thirdElfRucksack = elfGroup[2].compartments[0].concat(
+    elfGroup[2].compartments[1]
+  );
+
+  const firstAndSecondElvesCommonItems: string[] = [];
+
+  for (let i = 0; i < firstElfRucksack.length; i++) {
+    const item = firstElfRucksack.charAt(i);
+    if (secondElfRucksack.search(item) > -1) {
+      firstAndSecondElvesCommonItems.push(item);
+    }
+  }
+
+  for (let i = 0; i < firstAndSecondElvesCommonItems.length; i++) {
+    const item = firstAndSecondElvesCommonItems[i];
+    if (thirdElfRucksack.search(item) > -1) {
+      return item;
+    }
+  }
+};
+
+const computeElfGroupPriority = (elfGroupsList: ElfGroup[]): number => {
+  let totalPriority = 0;
+  elfGroupsList.forEach((elfGroup) => {
+    const item = findCommonItemInElvGroup(elfGroup);
+    totalPriority += getItemPriority(item);
+  });
+  return totalPriority;
+};
+
 async function main() {
   const lines = await getLinesOfFile("03/input.txt");
   const rucksacksList = createRucksacksList(lines);
   const totalPriority = computeTotalLostItemPriorities(rucksacksList);
-  console.log(totalPriority);
+  console.log("Lost item priority", totalPriority);
+  const ElvesGroups = createElvGroups(rucksacksList);
+  const elvesGroupPriority = computeElfGroupPriority(ElvesGroups);
+  console.log("Elves group priority", elvesGroupPriority);
 }
 
 main();
