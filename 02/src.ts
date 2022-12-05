6525f033fcb5c93d56261a088e8150247ae21a84
marineshaw <marines@bam.tech>
2022-12-05 21:34:21 +0100

    :recycle: (DayTwo) Refacto file for better reading

--- 02/src.ts
+++ 02/src.ts
@@ -19,6 +19,8 @@ const createFightList = (lines: string[]): Fight[] => {
   return fightList;
 };
 
+// First strategy
+
 const rockPaperScissorsValue: Record<string, number> = { X: 1, Y: 2, Z: 3 };
 const rockPaperScissorsOpponentValue: Record<string, number> = {
   A: 1,
@@ -26,50 +28,49 @@ const rockPaperScissorsOpponentValue: Record<string, number> = {
   C: 3,
 };
 
-const fightResultMatrix = [
+const firstStrategyFightResultMatrix = [
   [3, 0, 6],
   [6, 3, 0],
   [0, 6, 3],
 ];
 
 const computeFightScoreFirstStrat = (fight: Fight): number => {
+  const myValue = rockPaperScissorsValue[fight.me];
+  const opponentValue = rockPaperScissorsOpponentValue[fight.opponent];
   const resultValue =
-    fightResultMatrix[rockPaperScissorsValue[fight.me] - 1][
-      rockPaperScissorsOpponentValue[fight.opponent] - 1
-    ];
+    firstStrategyFightResultMatrix[myValue - 1][opponentValue - 1];
 
-  return rockPaperScissorsValue[fight.me] + resultValue;
+  return myValue + resultValue;
 };
 
-const computeTotalScore = (fightStrategy: Fight[]): number => {
-  let totalScore = 0;
-  fightStrategy.forEach((fight) => {
-    totalScore += computeFightScoreFirstStrat(fight);
-  });
-  return totalScore;
-};
+// Second strategy
 
 const fightResultValue: Record<string, number> = { X: 0, Y: 3, Z: 6 };
 
-const secondStratFightResultMatrix = [
+const secondStratRockPaperScissorsMatrix = [
   [3, 1, 2],
   [1, 2, 3],
   [2, 3, 1],
 ];
 
 const computeFightScoreSecondStrat = (fight: Fight): number => {
+  const myValue = rockPaperScissorsValue[fight.me];
+  const opponentValue = rockPaperScissorsOpponentValue[fight.opponent];
   const choiceValue =
-    secondStratFightResultMatrix[rockPaperScissorsValue[fight.me] - 1][
-      rockPaperScissorsOpponentValue[fight.opponent] - 1
-    ];
+    secondStratRockPaperScissorsMatrix[myValue - 1][opponentValue - 1];
 
   return fightResultValue[fight.me] + choiceValue;
 };
 
-const computeTotalScoreSecondStrat = (fightStrategy: Fight[]): number => {
+// Global
+
+const computeTotalScore = (
+  fightStrategy: Fight[],
+  computeFunction: (fight: Fight) => number
+): number => {
   let totalScore = 0;
   fightStrategy.forEach((fight) => {
-    totalScore += computeFightScoreSecondStrat(fight);
+    totalScore += computeFunction(fight);
   });
   return totalScore;
 };
@@ -77,8 +78,14 @@ const computeTotalScoreSecondStrat = (fightStrategy: Fight[]): number => {
 async function main() {
   const lines = await getLinesOfFile("02/input.txt");
   const fightList = createFightList(lines);
-  const scoreFirstStrat = computeTotalScore(fightList);
-  const scoreSecondStrat = computeTotalScoreSecondStrat(fightList);
+  const scoreFirstStrat = computeTotalScore(
+    fightList,
+    computeFightScoreFirstStrat
+  );
+  const scoreSecondStrat = computeTotalScore(
+    fightList,
+    computeFightScoreSecondStrat
+  );
   console.log("Score with the first strategy", scoreFirstStrat);
   console.log("Score with the second strategy", scoreSecondStrat);
 }
