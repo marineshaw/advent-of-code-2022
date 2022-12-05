aca3c71180a9032dcbee0ab18059ea91b70f72cc
marineshaw <marines@bam.tech>
2022-12-05 21:25:44 +0100

    :sparkles: (DayTwo) Part 2

--- 02/src.ts
+++ 02/src.ts
@@ -49,13 +49,38 @@ const computeTotalScore = (fightStrategy: Fight[]): number => {
   return totalScore;
 };
 
+const fightResultValue: Record<string, number> = { X: 0, Y: 3, Z: 6 };
+
+const secondStratFightResultMatrix = [
+  [3, 1, 2],
+  [1, 2, 3],
+  [2, 3, 1],
+];
+
+const computeFightScoreSecondStrat = (fight: Fight): number => {
+  const choiceValue =
+    secondStratFightResultMatrix[rockPaperScissorsValue[fight.me] - 1][
+      rockPaperScissorsOpponentValue[fight.opponent] - 1
+    ];
+
+  return fightResultValue[fight.me] + choiceValue;
+};
+
+const computeTotalScoreSecondStrat = (fightStrategy: Fight[]): number => {
+  let totalScore = 0;
+  fightStrategy.forEach((fight) => {
+    totalScore += computeFightScoreSecondStrat(fight);
+  });
+  return totalScore;
+};
+
 async function main() {
   const lines = await getLinesOfFile("02/input.txt");
   const fightList = createFightList(lines);
-  const firstScore = computeFightScoreFirstStrat(fightList[0]);
-  console.log(firstScore);
-  const score = computeTotalScore(fightList);
-  console.log("Score", score);
+  const scoreFirstStrat = computeTotalScore(fightList);
+  const scoreSecondStrat = computeTotalScoreSecondStrat(fightList);
+  console.log("Score with the first strategy", scoreFirstStrat);
+  console.log("Score with the second strategy", scoreSecondStrat);
 }
 
 main();
