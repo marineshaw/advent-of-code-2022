7e86bdc3a2c6b02525fff82dbb2ce2a82bd47bf9
marineshaw <marines@bam.tech>
2022-12-07 11:38:30 +0100

    :sparkles: (DayFour) Part 1

---
+++ 04/src.ts
@@ -0,0 +1,57 @@
+import { getLinesOfFile } from "../utils/getLinesOfFile";
+
+interface Assignment {
+  beginningSection: number;
+  endingSection: number;
+}
+
+type ElfPair = [Assignment, Assignment];
+
+const createElfPairsList = (lines: string[]): ElfPair[] => {
+  const elfPair: ElfPair[] = [];
+  lines.forEach((line) => {
+    const elvesStringList = line.split(",");
+    const firstElfStringList = elvesStringList[0].split("-");
+    const firstElf: Assignment = {
+      beginningSection: parseInt(firstElfStringList[0]),
+      endingSection: parseInt(firstElfStringList[1]),
+    };
+    const secondElfStringList = elvesStringList[1].split("-");
+    const secondElf: Assignment = {
+      beginningSection: parseInt(secondElfStringList[0]),
+      endingSection: parseInt(secondElfStringList[1]),
+    };
+
+    elfPair.push([firstElf, secondElf]);
+  });
+  return elfPair;
+};
+
+const countUselessPairs = (elfPairsList: ElfPair[]): number => {
+  let total = 0;
+  elfPairsList.forEach((elfPair) => {
+    const firstElf = elfPair[0];
+    const secondElf = elfPair[1];
+    if (
+      firstElf.beginningSection <= secondElf.beginningSection &&
+      firstElf.endingSection >= secondElf.endingSection
+    ) {
+      total += 1;
+    } else if (
+      secondElf.beginningSection <= firstElf.beginningSection &&
+      secondElf.endingSection >= firstElf.endingSection
+    ) {
+      total += 1;
+    }
+  });
+  return total;
+};
+
+async function main() {
+  const lines = await getLinesOfFile("04/input.txt");
+  const elfPairs = createElfPairsList(lines);
+  const uselessPairsCount = countUselessPairs(elfPairs);
+  console.log("useless", uselessPairsCount);
+}
+
+main();
